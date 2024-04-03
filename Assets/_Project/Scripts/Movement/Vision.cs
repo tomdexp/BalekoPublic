2d12bcac414c676c1e9329635d4e15753b0ac49f
jojoket <jules.sarton-du-jonchay.auditeur@lecnam.net>
2024-04-03 14:13:13 +0200

    update bestiole pref

--- Assets/_Project/Scripts/Movement/Vision.cs
+++ Assets/_Project/Scripts/Movement/Vision.cs
@@ -7,6 +7,8 @@ public class Vision : MonoBehaviour
     public GameObject model;
     public float Fov;
     public float VisionRange;
+    public int VerticeNum = 3;
+    public LineRenderer Line;
 
 
     // Start is called before the first frame update
@@ -18,7 +20,21 @@ public class Vision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Line.positionCount = VerticeNum;
+        int iter = 15;
+        float baseAngle = Mathf.Deg2Rad * model.transform.eulerAngles.z;
+        Vector3[] points = new Vector3[iter + 3];
+
+        points[0] = Vector3.zero;
+        for (int i = 0; i <= iter; i++)
+        {
+            float angle = Mathf.Deg2Rad * (Fov / iter * i - Fov / 2);
+            Vector3 angleVect = new Vector3(Mathf.Cos(baseAngle + angle), Mathf.Sin(baseAngle + angle)) * VisionRange;
+            points[i + 1] = angleVect;
+        }
+        points[iter + 2] = Vector3.zero;
+
+        Line.SetPositions(points);
     }
 
 
