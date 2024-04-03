02822b3369affdfec21d5ba4278a25928aaceb94
Alvina <alvinadr24@gmail.com>
2024-04-03 10:16:26 +0200

    feat: bar value connected to hunger and health

--- Assets/_Project/Scripts/Bestiole.cs
+++ Assets/_Project/Scripts/Bestiole.cs
@@ -8,6 +8,8 @@ public class Bestiole : MonoBehaviour
     public Genome Genome;
     public Damageable Damageable;
     public Damageable Hungerable;
+    public UI_BarValue HealthBar;
+    public UI_BarValue HungerBar;
     public SpriteRenderer SpriteRenderer;
 
     public void Awake()
@@ -17,6 +19,8 @@ public class Bestiole : MonoBehaviour
             //Damageable.MaxHealth = _enemyData.MaxHealth; //will be used to setup current life on start
             Damageable.OnReduceValue.AddListener(OnDamaged);
             Damageable.OnZeroValue.AddListener(OnDead);
+            Hungerable.OnReduceValue.AddListener(OnHungered);
+            Hungerable.OnZeroValue.AddListener(OnHungerDead);
             //Hungerable.MaxValue = _enemyData.MaxHealth; //will be used to setup current life on start
         }
     }
@@ -28,13 +32,25 @@ public class Bestiole : MonoBehaviour
 
     public void OnDamaged(float damage)
     {
-        SpriteRenderer.transform.DOScale(1f, .1f).OnComplete(() =>
+        SpriteRenderer.transform.DOScale(0.75f, .1f).OnComplete(() =>
         {
             SpriteRenderer.transform.DOScale(0.5f, .1f);
         });
+        HealthBar.SetBarValue(Damageable.CurrentValue, Damageable.MaxValue);
     }
 
     public void OnDead()
+    {
+        Destroy(gameObject);
+    }
+
+    public void OnHungered(float damage)
+    {
+        HungerBar.SetBarValue(Hungerable.CurrentValue, Hungerable.MaxValue);
+
+    }
+
+    public void OnHungerDead()
     {
         Destroy(gameObject);
     }
--- Assets/_Project/Scripts/UI/UI_BarValue.cs
+++ Assets/_Project/Scripts/UI/UI_BarValue.cs
@@ -5,10 +5,12 @@ using UnityEngine.UI;
 
 public class UI_BarValue : MonoBehaviour
 {
-    private Image _image;
+    [SerializeField] private Image _image;
+
     public void SetBarValue(float currentValue, float maxValue)
     {
         RectTransform rect = (RectTransform) _image.transform;
         rect.anchorMax = new Vector2(currentValue / maxValue, 1);
+        Debug.Log(currentValue / maxValue);
     }
 }
